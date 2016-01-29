744e6337e8acf3725857f862671d1b5cfee5fe26
George Singer <george.w.singer@gmail.com>
2016-01-29 17:17:29 -0500

    Installed npm prompt.

--- index.js
+++ index.js
@@ -2,11 +2,27 @@
 
 var min = require('minimist');
 var argv = min(process.argv.slice(2));
-var exec = require('child_process').exec;
+//var exec = require('child_process').exec;
 
 //var one = argv["_"][0] ? argv["_"][0] : "1.day";
 //var two = argv["_"][1] ? argv["_"][1] : "~";
 
-function puts(error, stdout, stderr) { console.log(stdout) };
+//function puts(error, stdout, stderr) { console.log(stdout) };
+//exec("./gg-log.sh " + one + " " + two, puts);
 
-exec("./gg-log.sh " + one + " " + two, puts);
+var prompt = require('prompt');
+
+prompt.start();
+
+prompt.get(['username', 'email'], function (err, result) {
+  if (err) { return onErr(err); }
+  console.log('Command-line input received:');
+  console.log('  Username: ' + result.username);
+  console.log('  Email: ' + result.email);
+});
+
+function onErr(err) {
+  console.log(err);
+  return 1;
+}
+
