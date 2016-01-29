728867523036e46bc26ad40086896568cd11b555
George Singer <george.w.singer@gmail.com>
2016-01-29 18:45:05 -0500

    Drafted prompt module workable template.

--- index.js
+++ index.js
@@ -11,14 +11,32 @@ var argv = min(process.argv.slice(2));
 //exec("./gg-log.sh " + one + " " + two, puts);
 
 var prompt = require('prompt');
+//prompt.message = "prompt (default value)";
+//prompt.delimiter = "";
+
+
+var schema = {
+  properties: {
+    ssh1: {
+		  description: 'ssh1 description',
+      message: 'ssh1 message',
+      default: 'george@192.168.1.2'
+    },
+    port1: {
+		  description: 'Port1 description',
+      message: 'Port1 message',
+      default: '58002'
+    }
+  }
+};
 
 prompt.start();
 
-prompt.get(['username', 'email'], function (err, result) {
+prompt.get(schema, function (err, result) {
   if (err) { return onErr(err); }
   console.log('Command-line input received:');
-  console.log('  Username: ' + result.username);
-  console.log('  Email: ' + result.email);
+  console.log('  ssh1: ' + result.ssh1);
+  console.log('  port1: ' + result.port1);
 });
 
 function onErr(err) {
