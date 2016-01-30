9e73424694f85cb0d5b1325c29b85b3f55f67564
George Singer <george.w.singer@gmail.com>
2016-01-30 16:33:34 -0500

    Corrected for LS3 logic.

--- ensure-sync.sh
+++ ensure-sync.sh
@@ -1,35 +1,17 @@
 #! /bin/bash
 
-> "./not-synced"
-
 SSH1=$1
 DIR1=$2
 SSH2=$3
 DIR2=$4
-#
-#echo "SSH1: " $1
-#echo "DIR1: " $2
-#echo "SSH2: " $3
-#echo "DIR2: " $4
 
-##SSH1=""
-#SSH1="ssh -p 58002 george@192.168.1.2"
-
-##SSH2=""
-#SSH2="ssh -p 58005 george@192.168.1.5"
-#DIR1="/home/george/Dropbox"
-#DIR2="/home/george/Dropbox"
+> "./ls1"
+> "./ls2"
+> "./ls3"
 
 LS1="./ls1"
 LS2="./ls2"
 
-#echo "Gathering list of files to compare (this may take a few seconds)."
-
-# ls -1a -d -1 $PWD/{*,.*}
-###"$SSH1" "ls -1a -d -1 $DROPBOX/{*,.*} > $DROPBOX/hash/ArchBox_dirs"
-
-#$SSH1 "ls -1a -d -1 $DIR1/{*,.*}" > $LS1
-#$SSH2 "ls -1a -d -1 $DIR2/{*,.*}" > $LS2
 if [ "$SSH1" = "local" ]; then
   #ls -1a -d -1 $DIR1/{*,.*} > $LS1
   ls -1a -d -1 $(realpath $DIR1)/{*,.*} > $LS1
@@ -48,16 +30,20 @@ fi
 cat $LS1 | sort -o $LS1
 cat $LS2 | sort -o $LS2
 
-# Delete first two lines of each file (noise)
+# Delete first two lines of each file (i.e., ignore the '.' and '..' symlinks)
 sed -i -e 1,2d $LS1
 sed -i -e 1,2d $LS2
+
+awk '{
+		print
+		getline line < second
+		if ($0 != line) print line
+}' second=$LS2 $LS1 > './ls3'
 
 function check_input_dir_file_for_hash_consistencies() {
 
 	while read line
 	do
-		#hashThroughSSH1="$($SSH1 "find \"$line\" -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum" < /dev/null)"
-		#hashThroughSSH2="$($SSH2 "find \"$line\" -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum" < /dev/null)"
 
     if [ "$SSH1" = "local" ]; then
       hashThroughSSH1=$(find "$line" -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum)
@@ -77,14 +63,14 @@ function check_input_dir_file_for_hash_consistencies() {
 		if [ "$hashThroughSSH1" == "$hashThroughSSH2" ]
 		then
 			echo "$line is synced"
-			continue # safeguard against my own stupidity
+			continue
 		else
 			echo "$line is not synced" # safeguard against my own stupidity
       echo "$line" >> "./not-synced"
-			# break
 		fi
 	done
 
 }
 
-cat $LS1 | check_input_dir_file_for_hash_consistencies
+LS3='./ls3'
+cat $LS3 | check_input_dir_file_for_hash_consistencies
--- index.js
+++ index.js
@@ -4,10 +4,6 @@ var min = require('minimist');
 var argv = min(process.argv.slice(2));
 var exec = require('child_process');
 
-//var one = argv["_"][0] ? argv["_"][0] : "1.day";
-//var two = argv["_"][1] ? argv["_"][1] : "~";
-
-
 var prompt = require('prompt');
 prompt.message = "";
 prompt.delimiter = "";
@@ -17,23 +13,15 @@ var schema = {
   properties: {
     targetSSH: {
       description: '    ssh (i.e., user@192.168.1.2):'
-      //message: 'ssh1 message',
-      //default: 'george@192.168.1.2'
     },
     targetPort: {
       description: '    port (i.e., 22):'
-      //message: 'Port1 message',
-      //default: '58002'
     },
     sourceDir: {
       description: '    source directory (use full path i.e., /home/user/Dropbox):'
-      //message: 'Port1 message',
-      //default: '58002'
     },
     targetDir: {
       description: '    target directory (use full path i.e., /home/user/Dropbox):'
-      //message: 'Port1 message',
-      //default: '58002'
     }
   }
 };
@@ -43,20 +31,11 @@ prompt.start();
 
 prompt.get(schema, function (err, result) {
   if (err) { return onErr(err); }
-  /*
-  console.log('  ssh: ' + result.targetSSH);
-  console.log('  port: ' + result.targetPort);
-  console.log('  port: ' + result.targetDir);
-  */
-  //console.log('Ensuring ' + result.targetDir + ' is synced at ' + result.targetSSH + ":" + result.targetPort);
 
   console.log("Computing differences between folders (this may take several seconds)..");
 
   function puts(error, stdout, stderr) { console.log(stdout); };
-  //console.log("./ensure-sync.sh " + " local " + result.sourceDir + " " + "ssh -p " + result.targetPort + " " + result.targetSSH + " " + result.targetDir);
-  //exec("./ensure-sync.sh " + " local " + result.sourceDir + " " + "ssh -p " + result.targetPort + " " + result.targetSSH + " " + result.targetDir, puts);
   exec.execFile("./ensure-sync.sh", ["local", result.sourceDir, "ssh -p " + result.targetPort + " " + result.targetSSH, result.targetDir], puts);
-  //exec.spawn("./ensure-sync.sh", ["local", result.sourceDir, "ssh -p " + result.targetPort + " " + result.targetSSH, result.targetDir]);
 });
 
 function onErr(err) {
